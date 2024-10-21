6593f7731d918993e733cf812211d1e400a8d1b4
Lenni <87639068+Lenni-builder@users.noreply.github.com>
2024-10-22 01:23:35 +0200

    add windows installer/run batch files
    entirely untested, please report any issues with the scripts or saffari to me either here or on discord. feel free to improve the scripts by opening a PR

---
+++ install.bat
@@ -0,0 +1,18 @@
+@echo off
+echo.
+echo This is a very barebones script to install Saffari for decstop on Windows. It has basically not been tested at all.
+echo If you want to improve it or report issues with it, open a GH PR, issue or report it on DC.
+echo You need to have Python 3 installed already for this to work, we might add a Python installer or bundle it at some point in the future.
+echo Press any key to continue...
+pause > nul
+cls
+echo.
+echo Installing requirements...
+echo.
+pip3 install -r requirements.txt
+echo.
+echo If there were no errors everything should be set up now.
+echo Open "run.bat" to start Saffari.
+echo Press any key to exit...
+pause > nul
+exit
---
+++ run.bat
@@ -0,0 +1,9 @@
+@echo off
+echo Starting Saffari...
+echo Everything you see below is either a Python Error (please include it if you report a crash), an error or warning with QtWebEngine or some other kind of log/warning/error.
+echo.
+python3 main.py
+echo.
+echo Saffari just exited. Either because you closed it or because it crashed.
+echo Press any key to exit...
+pause > nul
